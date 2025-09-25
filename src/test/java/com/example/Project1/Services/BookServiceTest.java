488abd3ba23bce7f2f001783b38f0e2ce4d32aa8
tejashwa_singh <tejashwa.singh@squareyards.co.in>
2025-09-25 22:17:27 +0530

    Added test case for the updateBook api

--- src/test/java/com/example/Project1/Services/BookServiceTest.java
+++ src/test/java/com/example/Project1/Services/BookServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.Project1.Services;
 
 import com.example.Project1.Controller.BookController;
+import com.example.Project1.Exceptions.BookNotFoundException;
 import com.example.Project1.Models.Author;
 import com.example.Project1.Models.Books;
 import com.example.Project1.Service.BookService;
@@ -12,6 +13,7 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.Date;
 
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -58,4 +60,36 @@ public class BookServiceTest {
 
         Assert.assertEquals(str, bookController.createBook(book).getTitle());
     }
+
+    @Test
+    public void updateBook() {
+        Date date = new Date();
+
+        Books book = new Books();
+        book.setId(1);
+        book.setPublishedDate(date);
+        book.setIsbn(111111L);
+        book.setTitle("Harry Potter");
+        book.setAuthor(new Author());
+
+        // Mock the service
+        when(bookService.updateBook(book)).thenThrow(new BookNotFoundException("Book Not Found"));
+
+        // Call controller (which uses the mocked service)
+        BookNotFoundException exception =
+                assertThrows(BookNotFoundException.class, () -> bookController.updateBook(book));
+
+        Assert.assertEquals("Book Not Found", exception.getMessage());
+    }
+
+    /* KEY Takeaways
+
+    -> Never mock the controller itself if you’re testing the controller → always mock its dependency (service).
+
+    -> Your error happened because the real controller was running, which hit an empty Optional in the repository.
+
+    -> Fix = mock the service (bookService) and let controller delegate to it.
+
+     */
+
 }

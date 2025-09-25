99938fbec0cc9855a21ce19b59eb76e732aa57c6
tejashwa_singh <tejashwa.singh@squareyards.co.in>
2025-09-25 21:38:58 +0530

    Added test case for the createBook api

--- src/test/java/com/example/Project1/Services/BookServiceTest.java
+++ src/test/java/com/example/Project1/Services/BookServiceTest.java
@@ -1,12 +1,16 @@
 package com.example.Project1.Services;
 
 import com.example.Project1.Controller.BookController;
+import com.example.Project1.Models.Author;
+import com.example.Project1.Models.Books;
 import com.example.Project1.Service.BookService;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Date;
 
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
@@ -35,5 +39,23 @@ public class BookServiceTest {
         int result = 6;
 
         Assert.assertEquals(result, bookController.addTwoNumbers(2,4));
+    }
+
+
+    @Test
+    public void createBookTest(){
+        Date date = new Date();
+
+        Books book = new Books();
+        book.setPublishedDate(date);
+        book.setIsbn(111111L);
+        book.setTitle("Harry Potter");
+        book.setAuthor(new Author());
+
+        when(bookController.createBook(book)).thenReturn(book);
+
+        String str = "Harry Potter";
+
+        Assert.assertEquals(str, bookController.createBook(book).getTitle());
     }
 }
